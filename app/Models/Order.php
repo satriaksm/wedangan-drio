065134da8902cf1d8578d5d022862cc2ccc92971
Satria Kusuma <satriaksm.20@gmail.com>
2024-11-18 16:48:14 +0700

    update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['user_id', 'total_items', 'total_price'];
+    protected $fillable = ['user_id', 'payment_method', 'total_items', 'total_price'];
     public function orderItems()
 {
     return $this->hasMany(OrderItem::class);
--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class OrdersSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class OrdersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('orders')->insert(
+            values: [
+            ['user_id' => 2, 'payment_method' => 'cash', 'total_items' => '13', 'total_price' => 152000],
+            ['user_id' => 2, 'payment_method' => 'cash', 'total_items' => '1', 'total_price' => 2000],
+            ['user_id' => 1, 'payment_method' => 'cash', 'total_items' => '3', 'total_price' => 15000],
+            ['user_id' => 1, 'payment_method' => 'cash', 'total_items' => '4', 'total_price' => 52000],
+            ],
+        );
     }
 }

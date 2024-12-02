53a930f8617d02a22cadc11bbe21192394a63d54
Satria Kusuma Mahardika <satriaksm.20@gmail.com>
2024-12-02 10:47:49 +0700

    minor changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use App\Exports\RiwayatTransaksiExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class OrderController extends Controller
 {
@@ -66,6 +68,55 @@ class OrderController extends Controller
                 'error' => 'Detail order tidak ditemukan',
                 'debug' => $e->getMessage()
             ], 500);
+        }
+    }
+
+    public function export_transactions(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'filter' => 'required|in:daily,weekly,monthly',
+            'date' => 'required|date'
+        ]);
+
+        // Tentukan nama file berdasarkan filter
+        $filename = 'laporan_transaksi_' . $request->filter . '_' .
+            Carbon::parse($request->date)->format('Y-m-d');
+
+        // Cek aksi yang dipilih
+        if ($request->action === 'pdf') {
+            // Proses download PDF
+            $query = Order::query();
+
+            switch ($request->filter) {
+                case 'daily':
+                    $query->whereDate('created_at', $request->date);
+                    $dateText = Carbon::parse($request->date)->format('d F Y');
+                    break;
+                case 'weekly':
+                    $startOfWeek = Carbon::parse($request->date)->startOfWeek();
+                    $endOfWeek = Carbon::parse($request->date)->endOfWeek();
+                    $query->whereBetween('created_at', [$startOfWeek, $endOfWeek]);
+                    $dateText = $startOfWeek->format('d F Y') . ' - ' . $endOfWeek->format('d F Y');
+                    break;
+                case 'monthly':
+                    $query->whereMonth('created_at', Carbon::parse($request->date)->month)
+                        ->whereYear('created_at', Carbon::parse($request->date)->year);
+                    $dateText = Carbon::parse($request->date)->format('F Y');
+                    break;
+            }
+
+            $orders = $query->get();
+
+            $mpdf = new \Mpdf\Mpdf();
+            $mpdf->WriteHTML(view('pages.history.table', compact('orders', 'dateText'))->render());
+            $mpdf->Output($filename . '.pdf', 'D');
+        } else if ($request->action === 'excel') {
+            // Proses download Excel
+            return Excel::download(
+                new RiwayatTransaksiExport($request->filter, $request->date),
+                $filename . '.xlsx'
+            );
         }
     }
 }
